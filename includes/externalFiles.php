b0d39032d25fb93389fa2c36dfb8a881b4b95296
SMBLegaspi <114278778+SMBLegaspi@users.noreply.github.com>
2022-09-30 18:55:11 +0800

    Update externalFiles.php

--- includes/externalFiles.php
+++ includes/externalFiles.php
@@ -1,4 +1,3 @@
 <link rel="stylesheet" href="assets/css/normalize.css">
 <link rel="stylesheet" href="assets/css/colors.css">
 <link rel="stylesheet" href="assets/css/styles.css">
-<link rel="stylesheet" href="assets/css/fonts.css">
